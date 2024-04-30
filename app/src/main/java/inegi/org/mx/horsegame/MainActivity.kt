e9fc216cb4948afa5c0a37e5a6180e08be559872
Alfredo10 <alfredo.marin@inegi.org.mx>
2024-04-30 16:36:43 -0600

    se gregan funciones para ocultar el mensaje de arriba y hacer que los cuadros del tablero sean cuadrados perfectos

--- app/src/main/java/inegi/org/mx/horsegame/MainActivity.kt
+++ app/src/main/java/inegi/org/mx/horsegame/MainActivity.kt
@@ -1,20 +1,57 @@
 package inegi.org.mx.horsegame
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.TypedValue
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TableRow
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+       // enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.rlScreen)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+       // ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.rlScreen)) { v, insets ->
+      //      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+       //     v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+       //     insets
+
+            initScreenGame()
         }
+
+        private fun initScreenGame(){
+            setSizeBord()
+            hideMessage()
+        }
+        //se cambian los tamaños de los cuadros del tablero en base a la resolución de la pantalla
+        private fun setSizeBord(){
+           var iv : ImageView
+           val display = windowManager.defaultDisplay
+           val size = Point()
+           display.getSize(size)
+           val width = size.x
+
+           var width_dp = (width / getResources().getDisplayMetrics().density)
+
+           var lateralMarginsDP = 0
+           val widthcell = (width_dp - lateralMarginsDP) / 8
+           val heigthcell = widthcell
+
+           for (i in 0..7)
+               for (j in 0..7){
+                   iv = findViewById(resources.getIdentifier("c$i$j", "id", packageName))
+
+                   var height = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,heigthcell,getResources().getDisplayMetrics()).toInt()
+                   var width  = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,widthcell,getResources().getDisplayMetrics()).toInt()
+                   iv.setLayoutParams(TableRow.LayoutParams(width, height))
+               }
+        }
+        private fun hideMessage(){
+            val lyMessage = findViewById<LinearLayout>(R.id.lyMessage)
+            lyMessage.visibility = View.INVISIBLE
+        }
+
     }
 }
