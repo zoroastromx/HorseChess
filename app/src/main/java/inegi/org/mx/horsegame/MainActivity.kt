7a12e502bfeaa6ca99418c8481d24b071af6fb2a
alfredo <alfredo.marin@inegi.org.mx>
2024-05-01 21:37:21 -0600

    matriz del tablero y pintar la celda que se le hace click

--- app/src/main/java/inegi/org/mx/horsegame/MainActivity.kt
+++ app/src/main/java/inegi/org/mx/horsegame/MainActivity.kt
@@ -11,8 +11,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
-    private var cellSelected_x = 0
-    private var cellSelected_y = 0
+    private var cellselectedX = 0
+    private var cellselectedY = 0
+
+    // hay que hacer una matriz del tablero para llevar el control de las celdas
+
+    private lateinit var board: Array<Array<Int>>
+    private var horseX = 0
+    private var horseY = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,26 +30,54 @@ class MainActivity : AppCompatActivity() {
         //     insets
 
         initScreenGame()
+        resetBoard()
         setFirstPosition() // para pintar de forma aleatoria la primera celda
     }
+
+    fun checkCell(v: View){
+        val name = v.tag.toString()
+        val x = name.subSequence(1,2).toString().toInt()
+        val y = name.subSequence(2,3).toString().toInt()
+
+        selectCell(x,y)
+
+
+        //val x = v.tag.toString().substring(1,2).toInt()
+        //val y = v.tag.toString().substring(2,3).toInt()
+
+    }
+
+    // hacer un reseteo del tablero
+    // cuando la celda esté en cero, significa que no hay caballo
+    // cuando la celda sea diferente de cero, significa que hay caballo
+
+    // valor 0 no hay caballo
+    // valor 1 hay caballo
+    // valor 2 es un bonus
+    // valor 9 es una opción del movimiento actualk
+    private fun resetBoard(){
+        board = Array(8) { Array(8) { 0 } }
+    }
+
      private fun setFirstPosition(){
             var x = 0
             var y = 0
             x = (0..7).random()
             y = (0..7).random()
 
-            cellSelected_x = x
-            cellSelected_y = y
+            cellselectedX = x
+            cellselectedY = y
             selectCell(x,y)
 
         }
     private fun selectCell(x: Int, y: Int){
               // hay que pintar el anterior de naranja
-                paintHorseCell(x,y, "previous_cell")
-                cellSelected_x = x
-                cellSelected_y = y
-                paintHorseCell(x,y, "selected_cell")
-            }
+        board[x][y] = 1
+        paintHorseCell(cellselectedX,cellselectedY, "previous_cell")
+        cellselectedX = x
+        cellselectedY = y
+        paintHorseCell(x,y, "selected_cell")
+        }
 
 
         private fun paintHorseCell(x: Int, y: Int, color: String){
